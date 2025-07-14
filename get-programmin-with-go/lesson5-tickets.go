efb7c9975718b575eac1e2a386b70cf669dd1ff6
King <24613526+R-D-King@users.noreply.github.com>
2025-07-14 14:06:36 -0700

    feat: add ticket generation program for space travel simulation

---
+++ get-programmin-with-go/lesson5-tickets.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	// header
+	fmt.Printf("%-20v %5v %-15v %10v\n", "SpaceLine", "Days", "Trip Type", "Price ($M)")
+	fmt.Println("=====================================================")
+
+	for i := 10; i > 0; i-- {
+		ticketGenerator()
+	}
+
+}
+
+func ticketGenerator() {
+
+	// // departure date
+	// year := 2020
+	// month := 10
+	// day := 13
+	hoursPerDay := 24
+	distance := 62100000        // km
+	speed := rand.Intn(14) + 16 // 16-30 km/s
+
+	price := 36 + (speed - 16)
+
+	// calculate the duration
+	duration := distance / (speed * (60 * 60 * hoursPerDay)) //in days
+	var spacelines string
+	var tripType string
+
+	switch companyID := rand.Intn(3); companyID {
+	case 0:
+		spacelines = "Space Adventures"
+	case 1:
+		spacelines = "SpaceX"
+	case 2:
+		spacelines = "Virgin Galactic"
+	}
+
+	switch tripCode := rand.Intn(2); tripCode {
+	case 0:
+		tripType = "one-way"
+	case 1:
+		tripType = "Round-trip"
+		price *= 2
+	}
+
+	fmt.Printf("%-20v %5v %v %-15v $%4v\n", spacelines, duration, "days", tripType, price)
+}
